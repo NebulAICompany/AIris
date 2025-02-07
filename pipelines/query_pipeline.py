03412f8a885cf70614fd68ca85138e460d3fe291
yAquila <yusufkartal2004@gmail.com>
2025-02-07 22:08:10 +0300

    Initial pipelines are complete.

---
+++ pipelines/query_pipeline.py
@@ -0,0 +1,68 @@
+from langchain_community.vectorstores import FAISS
+from openai import OpenAI
+from langchain_openai.embeddings import OpenAIEmbeddings
+
+class QueryPipeline:
+    def __init__(self, query: str, vectorstore_path: str):
+        self.query = query
+        self.vectorstore_path = vectorstore_path
+    def __find_similar_chunks(self):
+        # Retrieve the most similar semantic chunks
+        vectorstore = FAISS.load_local(f"{self.vectorstore_path}", embeddings=OpenAIEmbeddings(model="text-embedding-3-large"),allow_dangerous_deserialization=True)
+        chunks_query_retriever = vectorstore.as_retriever(search_kwargs={"k": 2})
+        similar_chunks = chunks_query_retriever.get_relevant_documents(self.query)
+        return similar_chunks
+
+    def __generate_prompt(self):
+        # Generate a prompt for the user
+
+        relevant_chunks = "\n".join([chunk.page_content for chunk in self.__find_similar_chunks()])
+        prompt = f"""Based on the just the following context, please provide a concise answer to the question.
+
+        Context:
+        {' '.join(relevant_chunks)}
+
+        Question: {self.query}
+
+        Answer: Let me answer based on the provided context."""
+        
+        return prompt
+    
+    def generate_response(self):
+        # Generate a response for the user
+        # Use OpenAI's API directly instead of local models
+        client = OpenAI(
+            api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
+        )
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",  
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": self.__generate_prompt()},
+            ],
+            max_tokens=150,
+            temperature=0.3,
+            top_p=0.9,
+        )
+        
+        return response.choices[0].message.content
+
+
+
+    # def save(self, similar_chunks: dict, save_path: str):
+    #     if save_path is None:
+    #         save_path = "query_results.txt"
+    #     with open(save_path, "w", encoding="utf-8") as f:
+    #         for chunk in similar_chunks:
+    #             f.write(f"Chunk: {chunk['text']}\n")
+    #             f.write(f"Similarity: {chunk['score']}\n\n")
+
+    #     print(f"\n✅ Query complete! Saved to '{save_path}'")
+
+import os
+os.environ["OPENAI_API_KEY"] = "***REMOVED***"  # Replace with your OpenAI key
+
+
+query_pipeline = QueryPipeline("madde-2yi açıkla", "vectorstore")  
+response = query_pipeline.generate_response()
+print(response)
