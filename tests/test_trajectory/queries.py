fe4bf5b1486be588fc96e4565d4cd98d0f4b2c18
essraAlp <ealparslan325@gmail.com>
2026-02-05 16:46:52 +0300

    test pipe removed

--- tests/test_trajectory/queries.py
+++
@@ -1,9 +0,0 @@
-# tests/test_queries.py
-TEST_QUERIES = [
-    "What is 2 + 2?",
-    "Elinde hangi dosyalar var.",
-    "Günlük döviz kuru nedir?",
-    "Apple hissesinin anlık değeri nedir",
-    "Apple hissesinin son bir haftalık performansını grafikle göster.",
-    "Son bir haftalık altın verilerinden excel dosyası oluştur.",
-]
